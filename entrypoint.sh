a37a2c6369e77d3afee1781ad96e40e57a37e0ee
Tobias Løfgren <tobloef@gmail.com>
2020-06-29 16:32:53 +0200

    Test puts

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,11 +41,13 @@ end
 coms = github.issue_comments(repo, pr_number)
 
 if check_duplicate_msg == "true"
+  puts "duplicate_msg_pattern: #{duplicate_msg_pattern}"
   if duplicate_msg_pattern != nil
     duplicate = coms.find { |c| (c["body"] =~ (Regexp.new duplicate_msg_pattern)) != nil }
   else
     duplicate = coms.find { |c| c["body"] == message }
   end
+  puts "duplicate: #{duplicate}"
 
   if duplicate
     puts "The PR already contains this message"
