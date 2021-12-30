f82a711e64e56cd2b3441c6289641955cb2a73cd
Aaron Klaassen <aaron@outerspacehero.com>
2021-12-30 16:31:54 -0600

    Fix duplicate message check so it happens even without other optional arguments.

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,27 +40,26 @@ else
   pr_number = pr["number"]
 end
 
-if !duplicate_msg_pattern.empty? || !delete_prev_regex_msg.empty?
-  comments = github.issue_comments(repo, pr_number)
 
-  if check_duplicate_msg == "true"
-    duplicate = if !duplicate_msg_pattern.empty?
-      comments.find { |c| c["body"].match(/#{duplicate_msg_pattern}/) }
-    else
-      comments.find { |c| c["body"] == message }
-    end
+comments = github.issue_comments(repo, pr_number)
 
-    if duplicate
-      puts "The PR already contains this message"
-      exit(0)
-    end
+if check_duplicate_msg == "true"
+  duplicate = if duplicate_msg_pattern&.empty?
+    comments.find { |c| c["body"] == message }
+  else
+    comments.find { |c| c["body"].match(/#{duplicate_msg_pattern}/) }
   end
 
-  if !delete_prev_regex_msg.empty?
-    comments.each do |comment|
-      if comment["body"].match(/#{delete_prev_regex_msg}/)
-        github.delete_comment(repo, comment["id"])
-      end
+  if duplicate
+    puts "The PR already contains this message"
+    exit(0)
+  end
+end
+
+if !delete_prev_regex_msg&.empty?
+  comments.each do |comment|
+    if comment["body"].match(/#{delete_prev_regex_msg}/)
+      github.delete_comment(repo, comment["id"])
     end
   end
 end
