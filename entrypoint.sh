b5c0af920742d306bed2e1999a511381f6b1afeb
Aaron Klaassen <aaron@outerspacehero.com>
2022-01-06 10:31:23 -0600

    Revert "Merge pull request #50 from unsplash/specs"
    This reverts commit 8f2c7d61c584467ec10c3ad0d8ca7f6aa780a600, reversing
    changes made to 481c213a70bad548cf59dc7130b736a56e706d9a.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,67 @@
 
 require "json"
 require "octokit"
-require_relative "lib/github"
-require_relative "lib/commenter"
-require_relative "lib/action"
 
-exit(run)
+json = File.read(ENV.fetch("GITHUB_EVENT_PATH"))
+event = JSON.parse(json)
+
+if !ENV["GITHUB_TOKEN"]
+  puts "Missing GITHUB_TOKEN"
+  exit(1)
+end
+
+github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+
+if ARGV[0].empty?
+  puts "Missing message argument."
+  exit(1)
+end
+
+message = ARGV[0]
+check_duplicate_msg = ARGV[1]
+delete_prev_regex_msg = ARGV[2]
+duplicate_msg_pattern = ARGV[3]
+
+repo = event["repository"]["full_name"]
+
+if ENV.fetch("GITHUB_EVENT_NAME") == "pull_request"
+  pr_number = event["number"]
+else
+  pulls = github.pull_requests(repo, state: "open")
+
+  push_head = event["after"]
+  pr = pulls.find { |pr| pr["head"]["sha"] == push_head }
+
+  if !pr
+    puts "Couldn't find an open pull request for branch with head at #{push_head}."
+    exit(1)
+  end
+  pr_number = pr["number"]
+end
+
+if !duplicate_msg_pattern.empty? || !delete_prev_regex_msg.empty?
+  comments = github.issue_comments(repo, pr_number)
+
+  if check_duplicate_msg == "true"
+    duplicate = if !duplicate_msg_pattern.empty?
+      comments.find { |c| c["body"].match(/#{duplicate_msg_pattern}/) }
+    else
+      comments.find { |c| c["body"] == message }
+    end
+
+    if duplicate
+      puts "The PR already contains this message"
+      exit(0)
+    end
+  end
+
+  if !delete_prev_regex_msg.empty?
+    comments.each do |comment|
+      if comment["body"].match(/#{delete_prev_regex_msg}/)
+        github.delete_comment(repo, comment["id"])
+      end
+    end
+  end
+end
+
+github.add_comment(repo, pr_number, message)
--- spec/spec_helper.rb
+++
@@ -1,5 +0,0 @@
-require "json"
-require "octokit"
-require_relative "../lib/github"
-require_relative "../lib/commenter"
-require_relative "../lib/action"
