3eb2037bc46b05a7d45b5933a762fe3a8803d74b
Ronald Baars <6582045+Ronald-Baars@users.noreply.github.com>
2022-03-10 13:59:56 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,7 @@ if !ENV["GITHUB_TOKEN"]
   exit(1)
 end
 
+github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
 message = ARGV[0]
 check_duplicate_msg = ARGV[1]
 delete_prev_regex_msg = ARGV[2]
